88a7f93fd9526ec3e52e8e992eed99e8e7705953
Viktor Konoplin <vkonoplin@newgistics.com>
2019-03-21 23:18:17 +0200

    Add Hosted Service

---
+++ src/VK.Consumer.Host/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+
+namespace VK.Consumer.Host
+{
+    public class App
+    {
+        private readonly CancellationToken _cancellationToken;
+
+        public App(IApplicationLifetime applicationLifetime)
+        {
+            _cancellationToken = applicationLifetime.ApplicationStopping;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello Consumer Hosted Service");
+        }
+    }
+}
---
+++ src/VK.Consumer.Host/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace VK.Consumer.Host
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureLogging((hostContext, config) =>
+                {
+                    config.AddConsole();
+                    config.AddDebug();
+                })
+                .ConfigureHostConfiguration(config =>
+                {
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    hostContext.HostingEnvironment.EnvironmentName =
+                        Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddSingleton<App>();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            using (host)
+            {
+                await host.StartAsync();
+
+                host.Services.GetRequiredService<App>().Run();
+
+                await host.WaitForShutdownAsync();
+            }
+        }
+    }
+}
